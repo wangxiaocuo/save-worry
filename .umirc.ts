884ed1b19a4d5f7117e70c0a96d8bf2b93ae50c9
Eli Wang <xiaocuo.wang@foxmail.com>
2022-08-02 11:04:07 +0800

    feat: 修改一些基础配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from 'dumi';
+import { defineConfig } from 'dumi'
 
-const repo = 'dumi-template';
+const repo = 'save-worry'
 
 export default defineConfig({
   title: repo,
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/logo.png',
+  logo: '/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   hash: true,
   // Because of using GitHub Pages
   base: `/${repo}/`,
   publicPath: `/${repo}/`,
+  locales: [['zh-CN', '中文']],
   navs: [
     null,
     {
       title: 'GitHub',
-      path: 'https://github.com/umijs/dumi-template',
-    },
-  ],
+      path: 'https://github.com/wangxiaocuo/save-worry'
+    }
+  ]
   // more config: https://d.umijs.org/config
-});
+})
