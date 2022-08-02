4c4a3ff7b222f18265ae84d069a55b3e90bde785
wangxiaocuo <wangxiaocuo@users.noreply.github.com>
2022-08-02 09:44:30 +0000

    deploy: 97835e2cc53ca29489b10017af7f27d18efebab6

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as listToTree } from './listToTree';
-export { default as treeToList } from './treeToList';
-export { default as traversalTree } from './traversalTree';
-export { default as findTreeNode } from './findTreeNode';
-export { default as findTreePath } from './findTreePath';
-export { default as trimTree } from './trimTree';
